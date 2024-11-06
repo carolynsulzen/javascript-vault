4345c9f806caca02c51e558dc466b87cc0664bb8
carolyn sulzen <carolynsulzen@carolyns-MacBook-Pro.local>
2024-11-05 21:38:24 -0700

    create javascript and html for vault code

---
+++ index.js
@@ -0,0 +1,14 @@
+// create an alert with a message for the user
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`)
+//create an alert with the lock combination
+alert(`10-40-39`)
+
+//create a variable with the first result being 10
+const firstNumber = 5 * 2;
+console.log(firstNumber);
+//create a variable with the second result being 40
+const secondNumber = 50 - 10;
+console.log(secondNumber);
+//create a varable with the third result being 39
+const thirdNumber = 30 + 9;
+console.log(thirdNumber);
